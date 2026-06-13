dfe1fe3e036f4839d5678a779ec0e47bfba77cd4
Jeroným Drvota <jeronym.drvota@dekra.com>
2026-06-13 21:58:50 +0200

    Vytváření screenshotu

--- ZxSpectrum/obj/Debug/net8.0/ZxSpectrum.AssemblyInfo.cs
+++ ZxSpectrum/obj/Debug/net8.0/ZxSpectrum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZxSpectrum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df88f68f0b36b1601baa5d686a0a563d61da371d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZxSpectrum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZxSpectrum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
